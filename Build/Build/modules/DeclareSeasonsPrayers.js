cf87cfc0bc01775d42eb6c87ff26d918060a7a0a
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2023-04-29 11:04:15 +0200

    Add files via upload

--- Build/Build/modules/DeclareSeasonsPrayers.js
+++ Build/Build/modules/DeclareSeasonsPrayers.js
@@ -1,6 +1,6 @@
 const giakiAll = [
     {
-        Season: 'Normal',
+        Season: Seasons.NoSeason,
         AR: 'أتيت',
         CA: 'آك إي',
         FR: 'es venus',
@@ -8,18 +8,42 @@ const giakiAll = [
         COP: 'ⲁⲕ̀\''
     },
     {
-        Season: 'Resurrection',
-        AR: 'قمت',
+        Season: Seasons.Nativity,
+        AR: 'ولِدت',
+        CA: 'آك ماسف',
+        FR: 'es né',
+        EN: '\'ve born',
+        COP: 'ⲁⲕ̀\''
+    },
+    {
+        Season: Seasons.Baptism,
+        AR: 'اعتَمَدُت',
+        CA: 'آك أومس',
+        FR: 'es baptisé',
+        EN: '\'ve been baptized',
+        COP: 'ⲁⲕ̀\''
+    },
+    {
+        Season: Seasons.PentecostalDays,
+        AR: 'قُمتَ',
         CA: 'آك تونك',
         FR: 'es ressuscité',
         EN: '\'ve raised',
         COP: 'ⲁⲕ̀\''
-    }
+    },
+    {
+        Season: Seasons.CrossFeast,
+        AR: 'صُلِبتَ',
+        CA: 'آك آشك',
+        FR: 'a été crucifié',
+        EN: '\'ve been crossed',
+        COP: 'ⲁⲕ̀\''
+    },
 ];
 const closingHymnAll = [
     //This is the variable part of 'Amin Alleluia Zoksa Patri...' closing hymn
     {
-        Season: 'Normal',
+        Season: Seasons.NoSeason,
         AR: '',
         CA: '',
         FR: '',
@@ -27,7 +51,7 @@ const closingHymnAll = [
         COP: ''
     },
     {
-        Season: 'Resurrection',
+        Season: Seasons.PentecostalDays,
         AR: '',
         CA: '',
         FR: '',
@@ -38,11 +62,16 @@ const closingHymnAll = [
 let giaki = setSeasonalText(giakiAll);
 let closingHym = setSeasonalText(closingHymnAll);
 function setSeasonalText(arrayAll) {
-    if (Season == Seasons.PentecostalDays) {
-        return arrayAll[1];
+    let prayer = arrayAll.filter(resp => resp.Season == Season);
+    if (prayer) {
+        return prayer[0];
     }
-    else {
-        return arrayAll[0];
+    else if ((copticReadingsDate == copticFeasts.PalmSunday && todayDate.getHours() > 15)
+        || HolyWeek.indexOf(copticReadingsDate) > -1) {
+        return arrayAll.filter(resp => resp.Season == Seasons.CrossFeast)[0];
+    }
+    else if (!prayer) {
+        return arrayAll.filter(resp => resp.Season == Seasons.NoSeason)[0];
     }
 }
 ;
