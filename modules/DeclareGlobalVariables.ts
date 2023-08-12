39e94c61467b59e2c5d25b30c7be833ae52a2b0e
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2023-08-12 09:13:19 +0200

    Add files via upload

--- modules/DeclareGlobalVariables.ts
+++ modules/DeclareGlobalVariables.ts
@@ -26,7 +26,7 @@ type typeButton = {
     any?: any
 };
 //CONSTANTS
-const version: string = 'v4.6.2 (Changed the musical note from eigth note to beamed note and made changes to the function formatting the note)';
+const version: string = 'v4.6.3 (Fixes to the date boxes. Fixes to some prayers)';
 const calendarDay: number = 24 * 60 * 60 * 1000; //this is a day in milliseconds
 const containerDiv: HTMLDivElement = document.getElementById('containerDiv') as HTMLDivElement;
 const leftSideBar: HTMLElement = document.getElementById('leftSideBar');
