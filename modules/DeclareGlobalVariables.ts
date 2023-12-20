ba5226ea98e206f5ee26c647040f9095188055dd
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2023-12-20 15:53:06 +0100

    Add files via upload

--- modules/DeclareGlobalVariables.ts
+++ modules/DeclareGlobalVariables.ts
@@ -23,7 +23,7 @@ type typeButton = {
     any?: any
 };
 //CONSTANTS
-const version: string = 'v5.0.4 (Fixes to the doxologies and added French version for some communion chants)';
+const version: string = 'v5.0.5 (Fixed Kiahk Fraction Prayer)';
 const calendarDay: number = 24 * 60 * 60 * 1000; //this is a day in milliseconds
 const containerDiv: HTMLDivElement = document.getElementById('containerDiv') as HTMLDivElement;
 const leftSideBar = document.getElementById('leftSideBar') as HTMLDivElement;
